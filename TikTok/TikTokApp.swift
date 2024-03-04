032b89e5511fa9bbb2286b3fc24e5adb363fddeb
Min Pyae <minmp2002@gmail.com>
2024-03-04 13:07:24 +0000

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Chris Min on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
